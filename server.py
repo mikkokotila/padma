7c7229a433c9ba966c3a96300a2d0ab3eff4f6e6
Mikko Kotila <mailme@mikkokotila.com>
2020-07-20 21:54:01 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    # Added host, port and debug, but I think only host is actually required (if default port is 5000 ?)
+    app.run(host='0.0.0.0', port=5000)
